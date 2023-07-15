8f1de48405338c854849320382dd30f46d73b76c
Akihiro Nishiyama <akih@keroling.net>
2023-07-15 16:22:02 -0700

    add env file support

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,28 @@
-const prod = process.env.NODE_ENV === 'production';
+const prod = process.env.NODE_ENV === "production";
+const fs = require("fs");
+const path = require("path");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
+
+var dotenvFiles = [`./.env.local`, `./.env`].filter(Boolean);
+
+dotenvFiles.forEach((dotenvFile) => {
+  if (fs.existsSync(dotenvFile)) {
+    require("dotenv").config({
+      path: dotenvFile,
+    });
+  }
+});
+
+// require("dotenv").config({ path: "./.env.local" });
 
 module.exports = {
-  mode: prod ? 'production' : 'development',
-  entry: './src/index.tsx',
+  mode: prod ? "production" : "development",
+  entry: "./src/index.tsx",
   output: {
-    path: __dirname + '/dist/',
+    path: __dirname + "/dist/",
   },
   module: {
     rules: [
@@ -15,22 +30,24 @@ module.exports = {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         resolve: {
-          extensions: ['.ts', '.tsx', '.js', '.json'],
+          extensions: [".ts", ".tsx", ".js", ".json"],
         },
-        use: 'ts-loader',
+        use: "ts-loader",
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
-    ]
+    ],
   },
-  devtool: prod ? undefined : 'source-map',
+  devtool: prod ? undefined : "source-map",
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: "./public/index.html",
     }),
     new MiniCssExtractPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(process.env),
+    }),
   ],
 };
-
