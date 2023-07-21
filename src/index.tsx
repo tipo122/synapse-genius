2d76488795889afdde0be172630c23db402de8f9
Akihiro Nishiyama <akih@keroling.net>
2023-07-21 13:02:33 -0700

    move App location

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { createRoot } from 'react-dom/client'
-import App from "./components/App";
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-const container = document.getElementById('root')!
-const root = createRoot(container)
+const container = document.getElementById("root")!;
+const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
