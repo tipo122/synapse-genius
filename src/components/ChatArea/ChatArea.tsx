2155bd4293427ee6a8d36696f9da075dfbaae9c0
Akihiro Nishiyama <akih@keroling.net>
2023-08-05 16:00:49 -0700

    add ChatArea

---
+++ src/components/ChatArea/ChatArea.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Layout } from "antd";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const Canvas = () => {
+  return (
+    <Content>
+      <div>Chat area</div>
+    </Content>
+  );
+};
+
+export default Canvas;
