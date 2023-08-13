96bdcc37abf19b87c03362dbcfed996a4061f69d
Akihiro Nishiyama <akih@keroling.net>
2023-08-12 22:07:56 -0700

    add user and canvas types

---
+++ src/types/canvas.ts
@@ -0,0 +1,23 @@
+export interface Canvas {
+  uid: string;
+  user_id: string;
+  template_id: string;
+  copydata: {
+    strings: [strings: string];
+  };
+  bg_image_uid: string;
+  bg_image_prompt: string;
+  item_property: {
+    item_name: string;
+    item_category: string;
+    item_description: string;
+  };
+  campaign_property: {
+    campaign_name: string;
+    campaign_description: string;
+  };
+  collaborators?: [{ user_id: string }];
+  canvas_data: {};
+  create_dt?: Date;
+  update_dt?: Date;
+}
