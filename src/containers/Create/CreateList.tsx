7b0118b496d2164e7c2658d604c4f7b4d216474b
Akihiro Nishiyama <akih@keroling.net>
2023-11-09 13:17:48 -0800

    temporaly fix

--- src/containers/Create/CreateList.tsx
+++ src/containers/Create/CreateList.tsx
@@ -1,30 +1,20 @@
 import React, { useContext, useEffect } from "react";
-import { List, Card, Col, Row } from "antd";
-import { Button, Input, Layout, Typography, theme } from "antd";
+import { List, Card } from "antd";
+import { Typography } from "antd";
 import { CreateContext } from "./CreateContainer";
-import { Link, useNavigate, useParams } from "react-router-dom";
-import {
-  FabricJSCanvas,
-  useFabricJSEditor,
-} from "../../hooks/useFabricJSEditor";
-import { initialCanvasData, useCanvasData } from "@hooks/useCanvasData";
-import { collection, doc, getDoc, query } from "firebase/firestore";
-import { app, db, functions } from "../../firebase";
-import { getDownloadURL, getStorage, ref } from "firebase/storage";
+import { useNavigate, useParams } from "react-router-dom";
 
-const { Text, Title } = Typography;
+import { useCanvasData } from "@hooks/useCanvasData";
+import { app, functions } from "../../firebase";
+import { getStorage } from "firebase/storage";
 
 export const CreateList = () => {
   const storage = getStorage();
   const navigate = useNavigate();
-  const size = (i) => (i === 0 ? 2 : 1);
   const { templates, templateType, searchTemplate, setTemplates } =
     useContext(CreateContext);
   const { canvasId } = useParams();
-  const { canvasData, saveCanvasData, saveCanvasImageData } = useCanvasData(
-    canvasId ?? ""
-  );
-  // const { selectedObjects, editor, onReady } = useFabricJSEditor();
+  const { canvasData, saveCanvasData } = useCanvasData(canvasId ?? "");
 
   useEffect(() => {
     (async () => {
@@ -58,14 +48,8 @@ export const CreateList = () => {
 
   const handleClick = (templateId: string) => {
     (async () => {
-      // const templateName = `templates/${templateId}.svg`;
-      // const templateFileRef = ref(storage, templateName);
-      // const jsonurl = await getDownloadURL(templateFileRef);
-      // const result = await fetch(jsonurl);
-
-      const templateURL = `https://firebasestorage.googleapis.com/v0/b/${process.env.REACT_APP_FIREBASE_STORAGEBUCKET}/o/templates%2F${templateId}.svg?alt=media`;
-      // const templateURL = `http://127.0.0.1:5001/synapse-genius-dev-fbe11/us-central1/on_get_embedded_template?template_id=CZ5BXSCQHtcZxG8eP3br`
-
+      const templateURL = `${getFunctionPath()}?template_id=${templateId}&canvas_id=${canvasId}`;
+      console.log(templateURL);
       saveCanvasData({ ...canvasData, canvas_data: templateURL });
     })();
     navigate(`/canvas/${canvasId}`);
@@ -87,12 +71,10 @@ export const CreateList = () => {
               >
                 <Card.Meta />
                 <img
-                  // src={`https://firebasestorage.googleapis.com/v0/b/${process.env.REACT_APP_FIREBASE_STORAGEBUCKET}/o/templates%2F${item}.svg?alt=media`}
                   src={`${getFunctionPath()}?template_id=${item}&canvas_id=${canvasId}`}
                   width={190}
                   height={190}
                 />
-                {/* <FabricJSCanvas className="synapse-canvas" onReady={onReady} /> */}
               </Card>
             </List.Item>
           )}
