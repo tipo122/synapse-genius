4b4cf1f832d43fa814728d588421832d7c099a06
Akihiro Nishiyama <akih@keroling.net>
2023-11-13 10:46:57 -0800

    crean up

--- src/containers/Create/CreateList.tsx
+++ src/containers/Create/CreateList.tsx
@@ -23,7 +23,6 @@ export const CreateList = () => {
     canvasId ?? ""
   );
   const alreadyReading = useRef<boolean>(false);
-  // const ImageIds = useRef<string[]>([]);
   const ImageStore = useRef<{ id: string; image: string }[]>([]);
   const [ImageList, setImageList] = useState<{ id: string; image: string }[]>(
     []
@@ -68,11 +67,6 @@ export const CreateList = () => {
     alreadyReading.current = true;
   };
 
-  // useEffect(() => {
-  //   console.log(ImageStore);
-  //   setImageList([...ImageStore.current]);
-  // }, [ImageStore.current.length]);
-
   const handleClick = (templateId: string) => {
     ImageStore.current.push({ id: "test", image: "tets" });
     // (async () => {
@@ -110,113 +104,3 @@ export const CreateList = () => {
     </>
   );
 };
-
-// import React, { useContext, useEffect, useRef, useState } from "react";
-// import { List, Card, Col, Row } from "antd";
-// import { Button, Input, Layout, Typography, theme } from "antd";
-// import { CreateContext } from "./CreateContainer";
-// import { Link, useNavigate, useParams } from "react-router-dom";
-// import {
-//   FabricJSCanvas,
-//   useFabricJSEditor,
-// } from "../../hooks/useFabricJSEditor";
-// import { initialCanvasData, useCanvasData } from "@hooks/useCanvasData";
-// import { collection, doc, getDoc, query } from "firebase/firestore";
-// import { app, db, functions } from "../../firebase";
-// import { getDownloadURL, getStorage, ref } from "firebase/storage";
-// import { httpsCallable } from "firebase/functions";
-
-// export const CreateList = () => {
-//   const storage = getStorage();
-//   const navigate = useNavigate();
-//   const { templates, templateType, searchTemplate, setTemplates } =
-//     useContext(CreateContext);
-//   const { canvasId } = useParams();
-//   const { canvasData, saveCanvasData, saveCanvasImageData } = useCanvasData(
-//     canvasId ?? ""
-//   );
-//   const alreadyReading = useRef<boolean>(false);
-//   // const ImageIds = useRef<string[]>([]);
-//   const ImageStore = useRef<{ id: string; image: string }[]>([]);
-//   const [ImageList, setImageList] = useState<{ id: string; image: string }[]>(
-//     []
-//   );
-
-//   const getEmbeddedTemplate: ({
-//     template_id,
-//     canvas_id,
-//   }) => Promise<{ data: any }> = httpsCallable(
-//     functions,
-//     "on_get_embedded_template"
-//   );
-
-//   useEffect(() => {
-//     if (!alreadyReading.current) {
-//       alreadyReading.current = true;
-//       if (templates.length > 0) {
-//         setTemplateData(templates);
-//       } else {
-//         (async () => {
-//           const resultData = await searchTemplate({
-//             text_query: "",
-//             template_type: canvasData.template_property.template_type,
-//           });
-//           setTemplateData(resultData.data);
-//         })();
-//       }
-//     }
-//   }, [canvasData]);
-
-//   const setTemplateData = (idList) => {
-//     console.log(`settemplatedata: ${idList}`);
-//     idList.forEach(async (templateId, i) => {
-//       let image;
-//       ({ data: image } = await getEmbeddedTemplate({
-//         template_id: templateId,
-//         canvas_id: canvasId,
-//       }));
-//       ImageStore.current.push({ id: templateId, image: image });
-//     });
-//     alreadyReading.current = true;
-//   };
-
-//   // useEffect(() => {
-//   //   setImageList(ImageStore.current);
-//   // }, [ImageStore.current.length]);
-
-//   const handleClick = (templateId: string) => {
-//     (async () => {
-//       const templateURL = `https://firebasestorage.googleapis.com/v0/b/${process.env.REACT_APP_FIREBASE_STORAGEBUCKET}/o/templates%2F${templateId}.svg?alt=media`;
-//       saveCanvasData({ ...canvasData, canvas_data: templateURL });
-//     })();
-//     navigate(`/canvas/${canvasId}`);
-//   };
-
-//   return (
-//     <>
-//       <div style={{ width: "600px", textAlign: "right" }}>
-//         <br />
-//         <List
-//           grid={{ gutter: 16, column: 2 }}
-//           dataSource={ImageStore.current}
-//           renderItem={(item) => (
-//             <List.Item>
-//               <Card
-//                 hoverable
-//                 style={{ width: 240, height: 240 }}
-//                 onClick={() => handleClick(item.id)}
-//               >
-//                 <Card.Meta />
-//                 <img
-//                   src={`data:image/svg+xml;base64,${item.image}`}
-//                   width={190}
-//                   height={190}
-//                 />
-//               </Card>
-//             </List.Item>
-//           )}
-//         />
-//       </div>
-//     </>
-//   );
-// };
