b4e387ef06678b4e87bd0aa420d0fa811ed146c0
isamu <isamu@to-kyo.to>
2023-11-18 09:02:33 +0900

    add py_test/hello.py

---
+++ py_test/hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# python -m py_test.hello
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../functions"))
+
+from functions.create_template_elements import generate_openai_message 
+
+count = 5
+response = {}
+context = []
+res = generate_openai_message(count, response, context)
+
+print(res);
