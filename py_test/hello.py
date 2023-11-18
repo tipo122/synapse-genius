008e1d0e93272ba26f841ee14b632ec8ecbdf0a0
isamu <isamu@to-kyo.to>
2023-11-18 09:20:20 +0900

    format log

--- py_test/hello.py
+++ py_test/hello.py
@@ -2,14 +2,17 @@
 # python -m py_test.hello
 import os
 import sys
+import json
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../functions"))
 
 from functions.create_template_elements import generate_openai_message 
 
-count = 5
-response = {}
-context = []
-res = generate_openai_message(count, response, context)
+if __name__ == "__main__":
+    
+    count = 5
+    response = {}
+    context = []
+    res = generate_openai_message(count, response, context)
 
-print(res);
+    print(json.dumps(res, indent=2, ensure_ascii=False));
